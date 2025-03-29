bbea07dbd5035751ac1af414936b66f45c7e18da
Hungleee04 <114040651+Hungleee04@users.noreply.github.com>
2025-03-30 01:16:35 +0700

    h

--- src/components/AddComponent.jsx
+++ src/components/AddComponent.jsx
@@ -3,13 +3,30 @@ import {useState} from "react";
 export const AddComponent = (props) => {
     const {list, setList} = props;
     const[value , setValue] = useState("");
+    let obj = {};
     const handleAdd =() =>{
         if(value === ""){
                 return;
         }
-
-        setList([...list,value]);
+        obj = genOBJ(value)
+        setList([...list,obj]);
         setValue("");
+        obj = {};
+    }
+    let i = list.length +1;
+    const getRandomEven = (min, max) => {
+        let num;
+        do {
+            num = Math.floor(Math.random() * (max - min + 1)) + min;
+        } while (num % 2 !== 0);
+        return num;
+    };
+    const genOBJ = (value)=>{
+        return {
+            id : getRandomEven(1,10000000),
+            name : value,
+            index : i
+        }
     }
 
     return (
--- src/components/ListComponent.jsx
+++ src/components/ListComponent.jsx
@@ -3,7 +3,7 @@ const list = props.ds;
 
     return (
         <div>{list.map((item,index)=>{
-            return (<p key={index}>{item}</p>)
+            return (<p key={index}>{item.name}</p>)
         })}
         </div>
     )
